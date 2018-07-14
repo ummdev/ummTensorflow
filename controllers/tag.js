bb4603dae04dda978355b34c16dbdcbca65fa6d8
userkmitl <59050300@kmitl.ac.th>
2018-07-15 01:11:04 +0700

    :heart_eyes: fix : string

--- controllers/tag.js
+++ controllers/tag.js
@@ -1,7 +1,7 @@
 const firestore = require('./../models/firebase');
 const Tag = function (req, res) {
     firestore.collection('tag').doc('eVF8kddt3jXfGrjkDMF4').get().then((data) => {
-        const dataset = data.data()['0'].map((value) => Number(value))
+        const dataset = data.data()['0']
         const item = dataset[Math.floor(Math.random()*dataset.length)];
         res.status(200).json({ success: true, massage: item })
     })
