ebe6e297c803b3beb54b8f0634471bb8acf2db0e
userkmitl <59050300@kmitl.ac.th>
2018-07-15 00:23:59 +0700

    :heart_eyes: add : tag

--- controllers/tag.js
+++ controllers/tag.js
@@ -1,5 +1,4 @@
 const firestore = require('./../models/firebase');
-
 const Tag = function (req, res) {
     firestore.collection('tag').doc('eVF8kddt3jXfGrjkDMF4').get().then((data) => {
         const dataset = data.data()['0'].map((value) => Number(value))
